9bb78d289c1931407d55cbe893858c1f9e481bf5
Maria José Mira <100717019+Mirau25@users.noreply.github.com>
2023-10-02 14:23:58 -0500

    Add files via upload

---
+++ LOGIN-MERN/src/models/user.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        require : true,
+        trim : true
+
+    },
+    email : {
+        type : String,
+        unique : true,
+        require : true,
+        trim : true
+    },
+
+    password : {
+        type : String,
+        require : true
+    }
+},{
+    timestamps : true
+});
+
+export default mongoose.model("User", userSchema)
