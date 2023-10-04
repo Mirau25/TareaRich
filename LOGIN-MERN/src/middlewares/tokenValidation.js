c555d86fbadc1ca6a9711cb67b39e684ff57f56c
Maria José Mira <100717019+Mirau25@users.noreply.github.com>
2023-10-03 23:15:29 -0500

    Delete LOGIN-MERN directory

--- LOGIN-MERN/src/middlewares/tokenValidation.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { TOKEN_SECRET } from '../config';
-import { cookie } from 'express/lib/response';
-
-export const requireAuth = (req, res, next) =>{
-  //console.log(req.headers);
-  const { token } = req.cookies;
-  console.log(cookies);
-}
