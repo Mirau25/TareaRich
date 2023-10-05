06e952087ae90378f38978d6b35d1b7166a50471
Maria José Mira <100717019+Mirau25@users.noreply.github.com>
2023-10-05 17:40:47 -0500

    Add files via upload

---
+++ LOGIN MERN/src/middlewares/validator.middleware.js
@@ -0,0 +1,8 @@
+export const validateSchema = (Shema) => (req, res, next) => {
+    try{
+        Shema.parse(req.body);
+        next();
+    } catch (error) {
+        return res.status(400).json({ error})
+    }
+}
